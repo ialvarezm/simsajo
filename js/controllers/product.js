095c7aa2443d7801c6e256eec147a889064cabfa
ialvarezm <alvarez.isaias@gmail.com>
2016-10-09 21:17:15 -0600

    Editar producto

--- js/controllers/product.js
+++ js/controllers/product.js
@@ -1,13 +1,14 @@
-app.controller('productController', ['$scope', 'QueryService', 'Notification', 'ModalService', '$timeout',
-    function productController($scope, QueryService, Notification, ModalService, $timeout) {
+app.controller('productController', ['$scope', 'QueryService', 'Notification', '$timeout',
+    function productController($scope, QueryService, Notification, $timeout) {
         $scope.init = function () {
-            $scope.loading = true;
-            $scope.list = true;
             $('.tool').tooltip();
             getProducts();
+            getCategories();
         };
 
         var getProducts = function () {
+            $scope.list = true;
+            $scope.loading = true;
             QueryService.get('getProducts', {},
             function(response) {
                 $scope.products = response;
@@ -17,9 +18,42 @@ app.controller('productController', ['$scope', 'QueryService', 'Notification', '
             });
         };
 
-        $scope.showForm = function(edit){
-            $scope.list = false;
+        var getCategories = function () {
+            QueryService.get('getCategories', {},
+            function(response) {
+                $scope.categories = response;
+            });
         };
+
+        $scope.showForm = function(edit, product){
+            $scope.edit = edit;
+            if(edit) $scope.title = 'Editar Producto';
+            else $scope.title = 'Agregar Producto';
+            $scope.list = false;
+            $scope.product = product;
+        };
+
+        $scope.save = function(){
+            var url, message;
+            if($scope.edit) {
+                url = 'updateProduct';
+                message = 'Producto actualizado';
+            }else {
+                url = 'addProduct';
+                message = 'Producto guardado';
+            }
+            QueryService.post(url, $scope.product,
+            function(response) {
+                $scope.product =  {};
+                $scope.cancel();
+            });
+
+        };
+
+        $scope.cancel = function () {
+            $scope.list = true;
+            getProducts();
+        }
 
     }
 ]);
