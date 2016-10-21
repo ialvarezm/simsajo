d5a5d89018175b11a1213fd5d72a17838fedf592
ialvarezm <alvarez.isaias@gmail.com>
2016-10-21 09:14:36 -0600

    Mensaje

--- js/app.js
+++ js/app.js
@@ -30,7 +30,7 @@ app.controller('productsController', [
     '$scope',
     function productsController($scope) {
         $scope.items = [
-      {name: 'Bed', price: '699', oldprice: '760'},
+      {name: 'Cama', price: '699', oldprice: '760'},
       {name: 'Nightstand', price: '399', oldprice: '450'},
       {name: 'Hammock', price: '599', oldprice: '640'}
        ];
